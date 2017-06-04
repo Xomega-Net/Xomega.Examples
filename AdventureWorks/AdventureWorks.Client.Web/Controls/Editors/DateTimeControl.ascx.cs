bc97f7ed29f1d15d0c477449816682fd96ea97cc
Xomega.Net <misc@xomega.net>
2017-06-04 13:17:59 -0400

    Code for complete Xomega tutorial based on Adventure Works sample DB

---
+++ AdventureWorks/AdventureWorks.Client.Web/Controls/Editors/DateTimeControl.ascx.cs
@@ -0,0 +1,14 @@
+﻿using Xomega.Framework.Properties;
+using Xomega.Framework.Web;
+
+namespace AdventureWorks.Client.Web
+{
+    public partial class DateTimeControl : BaseDateTimeControl
+    {
+        public override void OnPropertyBound(DateTimeProperty prop)
+        {
+            base.OnPropertyBound(prop);
+            extCalendar.Format = prop.Format;
+        }
+    }
+}
---
+++ AdventureWorks/AdventureWorks.Client.Web/Controls/Editors/PickListControl.ascx.cs
@@ -0,0 +1,8 @@
+﻿using Xomega.Framework.Web;
+
+namespace AdventureWorks.Client.Web
+{
+    public partial class PickListControl : BasePickListControl
+    {
+    }
+}
